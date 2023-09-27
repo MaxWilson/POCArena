a72bad71b240beddb7d9812703df196ea360c707
Max Wilson <wilson.max@gmail.com>
2023-09-26 20:51:23 -0700

    Still wip.

--- src/Scratch/Behaviors.fsx
+++ src/Scratch/Behaviors.fsx
@@ -4,17 +4,11 @@
 let enemies = ["Bob", 10; "Joe", 14; "Sue", 6] |> Map.ofList
 
 type ActionResult = Success | Failure
-type ExecutionResult<'action, 'memory, 'ctx> = Finished of ActionResult | Yield | DoAction of 'action * 'memory * Behavior<'action, 'memory, 'ctx>
-and Behavior<'action, 'memory, 'ctx> = ActionResult * 'memory * 'ctx -> ExecutionResult<'action, 'memory, 'ctx>
-type ActionRequest<'action, 'memory> = ActionRequest of 'action * 'memory
+type ExecutionResult<'actionOut, 'memory, 'ctx> = Finished of ActionResult | Yield | DoAction of 'actionOut * 'memory * Behavior<'actionOut, 'memory, 'ctx>
+and Behavior<'actionOut, 'memory, 'ctx> = ActionResult * 'memory * 'ctx -> ExecutionResult<'actionOut, 'memory, 'ctx>
+type ActionRequest<'actionOut, 'memory> = ActionRequest of 'actionOut * 'memory
 type QueryRequest<'memory, 'ctx, 'result> = QueryRequest of ('memory * 'ctx -> 'result)
-type Action = class end // make these concrete at first just to make dev easier
-type Context = {
-    enemyHP: Map<string, int>
-    }
-type Memory = class end
-// In order to make this work, action and ctx have to be fixed for the whole tree, while memory has to be lensed.
-let bind (lhs: Behavior<Action, Memory, Context>) (rhs: Behavior<Action,_,Context>) : Behavior<Action,_,Context> =
+let bind (lhs: Behavior<'action, 'memory, 'context>) (rhs: Behavior<'action,_,'context>) : Behavior<'action, _, 'context> =
     fun (s, m, c) ->
         match lhs(s, m, c) with
         | Finished a -> rhs(a,m,c)
@@ -22,60 +16,79 @@ let bind (lhs: Behavior<Action, Memory, Context>) (rhs: Behavior<Action,_,Contex
         | DoAction(a, m', continuation) -> DoAction(a, m', notImpl())
 
 type BehaviorBuilder() =
-    member this.Return (x: ActionResult) : Behavior<_,_,_> = fun _ -> Finished x
+    member this.Return (x: ActionResult) = Finished x
     member this.ReturnFrom (x: Behavior<_,_,_>) = x
     member this.Bind(b, f) = bind b f
-    member this.Bind(ActionRequest(action, mem), bhv: Behavior<_,_,_>) =
+    member this.Bind(ActionRequest(action, mem), bhv: (ActionResult * 'memory * 'context -> ExecutionResult<_,_,_>)) =
         DoAction(action, mem, bhv)
     member this.Bind(b: QueryRequest<_,_,'result>, f: 'result -> Behavior<_,_,_>) =
         fun(a, m, c) ->
             let (QueryRequest qf) = b
             let r = qf(m,c)
             (f r)(a,m,c)
-    // member this.Delay(f) = f()
+    // member this.Delay(f): Behavior<_,_,_> = fun(action, memory, context) -> f()(action, memory, context)
     // member this.Zero() = fun _ -> None
     // member this.Run(b, s, m) = b(s, m)
 let behavior = BehaviorBuilder()
 
 type SimpleAction = Attack of name: string | MoveTo of name: string
+// type Action = class end // make these concrete at first just to make dev easier
+type Context = {
+    enemyHP: Map<string, int>
+    }
+type Memory = class end
+// In order to make this work, action and ctx have to be fixed for the whole tree, while memory has to be lensed.
 let attack (name: string) = Attack name
 type AttackMemory = {
     target: string option
     }
     with static member fresh = { target = None }
 
-let trivial() = behavior { return Success }
+let trivial: Behavior<_,_,_> = behavior {
+    return Failure
+    }
+let almosttrivial(): Behavior<_,_,_> = behavior {
+    let! (result: ActionResult, mem: Memory, ctx: Context) = ActionRequest(attack "Bob", notImpl())
+    return Failure
+    }
 
-let rec kill() = behavior {
+let quasitrivial = behavior {
+    let! (result: ActionResult, mem: Memory, ctx: Context) = ActionRequest(attack "Bob", notImpl())
+    return Failure
+    }
+
+let rec kill(): Behavior<_,_,_> = behavior {
     let! target = QueryRequest <| fun (mem: AttackMemory, ctx: Context) -> mem.target |> Option.orElseWith (fun () -> ctx.enemyHP |> Map.tryPick (fun name hp -> if hp > 0 then Some name else None))
     match target with
     | None -> return Success
     | Some target ->
         let mem = notImpl "attacking target"
         let! (result: ActionResult, mem: Memory, ctx: Context) = ActionRequest(attack target, mem)
-        let inflictedInjury result ctx = notImpl "detect whether the attack inflicted injury--more than just a simple success/failure check"
-        if result = Success && inflictedInjury result ctx then
-            // keep attacking
-            return! kill() // kill ctx // how to keep the state? Is recursion even the right way to continue here?
-        else return Failure // todo: try attacking a different target first
+        return Failure
+        // let inflictedInjury result ctx = notImpl "detect whether the attack inflicted injury--more than just a simple success/failure check"
+        // if result = Success && inflictedInjury result ctx then
+        //     // keep attacking
+        //     return! kill() // kill ctx // how to keep the state? Is recursion even the right way to continue here?
+        // else return Failure // todo: try attacking a different target first
     }
 
 let kill2 =
     fun (a, m, c) ->
-        let bind = notImpl
+        let bind(x,y) = notImpl()
         bind(Finished Success, fun target ->
             match target with
             | None -> Finished Success
             | Some target ->
                 let mem = notImpl "attacking target"
                 let q = ActionRequest(attack target, mem)
-                bind(q, fun (result, mem, ctx) ->
+                let bhv: Behavior<_,_,_> = (fun (result, mem, ctx) ->
                     let inflictedInjury result ctx = notImpl "detect whether the attack inflicted injury--more than just a simple success/failure check"
                     if result = Success && inflictedInjury result ctx then
                         // keep attacking
                         trivial()(result, mem, ctx) // kill ctx // how to keep the state? Is recursion even the right way to continue here?
                     else Finished Failure // todo: try attacking a different target first
-                    ))
+                    )
+                bind(q, bhv)
 
 
 (*
